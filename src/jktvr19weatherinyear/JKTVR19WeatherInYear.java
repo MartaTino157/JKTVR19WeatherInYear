daa3e18cef15a837c0f683172d8a02e5ea7aa11e
pupil <pupil@ivkhk.ee>
2020-09-15 11:18:56 +0300

    Сделали основную работу

--- src/jktvr19weatherinyear/JKTVR19WeatherInYear.java
+++ src/jktvr19weatherinyear/JKTVR19WeatherInYear.java
@@ -15,7 +15,8 @@ public class JKTVR19WeatherInYear {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        App app = new App();
+        app.run();
     }
     
 }
